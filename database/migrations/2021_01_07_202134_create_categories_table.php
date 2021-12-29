5423b0b5bd498646954ecba1980d7f75a8da5b55
WithRomance <withromance@hotmail.com>
2021-12-29 17:02:40 +0300

    Laravel Admin Panel Template Uyarlaması

--- database/migrations/2021_01_07_202134_create_categories_table.php
+++ database/migrations/2021_01_07_202134_create_categories_table.php
@@ -14,7 +14,7 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+            $table->id()->autoIncrement();
             $table->integer("parent_id")->default(0);
             $table->string("title",150);
             $table->string("keywords")->nullable(true);
