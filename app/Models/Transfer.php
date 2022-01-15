f0e00d8dbe61d0a2c948399481f952c96e52230b
WithRomance <withromance@hotmail.com>
2022-01-15 21:20:09 +0300

    Laravel User Admin Role Permission Yetkilendirmeler

--- app/Models/Transfer.php
+++ app/Models/Transfer.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Transfer extends Model
 {
     use HasFactory;
+    #Many to One
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function randevu(){
+        return $this->hasMany(Reservation::class);
+    }
 }
